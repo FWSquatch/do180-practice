5a2066edb5a299f2b3bc407e885996710e92e878
Tess Sluijter-Stek <tess@unixerius.nl>
2023-06-19 21:02:28 +0200

    Task 6 solution.

--- Solutions/task6.sh
+++ Solutions/task6.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+cp /dockerfiles/nginx/* .
+
+cat > ./task6.dockerfile << EOF
 # Edit this file to create an nginx webserver
 # Pull from centos:8
 FROM centos:8
@@ -19,3 +24,16 @@ COPY duffman.png /usr/share/nginx/html/duffman.png
 # Run the command 'nginx -g daemon off;'
 ENTRYPOINT ["/usr/sbin/nginx"]
 CMD ["-g", "daemon off;"]
+EOF
+
+podman build -f ./task6.dockerfile -t registry.do180.lab:5000/duff-nginx:1.0 .
+
+rm Dockerfile index.html duffman.png
+
+podman push registry.do180.lab:5000/duff-nginx:1.0
+
+podman run -d --name duffman \
+        -p 8989:80 \
+        registry.do180.lab:5000/duff-nginx:1.0
+
+curl http://localhost:8989
