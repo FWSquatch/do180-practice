d2dfb6904c198e93a10e4d459ae97448a1fc4f0b
Tess Sluijter-Stek <tess@unixerius.nl>
2026-04-07 21:41:44 +0200

    Update to task3.

--- Solutions/task3.sh
+++ Solutions/task3.sh
@@ -7,12 +7,15 @@ podman pull registry.do180.lab:5000/mariadb:latest
 podman run -d \
         --name testql \
         -p 3306:3306 \
-        -e MYSQL_USER=duffman \
-        -e MYSQL_PASSWORD=saysoyeah \
-        -e MYSQL_ROOT_PASSWORD=SQLp4ss \
-        -e MYSQL_DATABASE=beer \
+        -e MYSQL_USER="duffman" \
+        -e MYSQL_PASSWORD="saysoyeah" \
+        -e MYSQL_ROOT_PASSWORD="SQLp4ss" \
+        -e MYSQL_DATABASE="beer" \
         registry.do180.lab:5000/mariadb:latest
 
-echo "show databases;" | mysql -uduffman -h 192.168.88.4 -psaysoyeah
+echo "show databases;" | mysql -uduffman -psaysoyeah -h workstation
 
-mysql -uroot -h 192.168.88.4 -pSQLp4ss < /sql/beer.sql
+mysql -uroot -pSQLp4ss -h workstation < /sql/beer.sql
+
+echo 'select * from types' | mysql -uduffman -psaysoyeah -h workstation beer
+
