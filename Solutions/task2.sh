5952286d5a1efce0797df6c848324f9712047cac
Tess Sluijter-Stek <tess@unixerius.nl>
2026-04-07 21:17:12 +0200

    Modernisation of task2.sh

--- Solutions/task2.sh
+++ Solutions/task2.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-sudo podman pull registry.do180.lab:5000/httpd
+skopeo list-tags registry.do180.lab:5000/httpd
+
+sudo podman pull registry.do180.lab:5000/httpd:latest
 
 sudo podman run -d \
         --name reg-httpd \
         -p 8080:80 \
-        -v /web:/usr/local/apache2/htdocs \
-        registry.do180.lab:5000/httpd
-
-sudo semanage fcontext -a -t container_file_t /web
-sudo semanage fcontext -a -t container_file_t /web/index.html
-sudo restorecon /web
-sudo restorecon /web/index.html
+        -v /web:/usr/local/apache2/htdocs:Z \
+        registry.do180.lab:5000/httpd:latest
 
 sudo podman generate systemd reg-httpd | sudo tee -a /usr/lib/systemd/system/reg-httpd.service
 
